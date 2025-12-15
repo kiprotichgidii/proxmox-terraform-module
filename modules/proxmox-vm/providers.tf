5c56640c631764e4730b2f0e2caf5e9f3eb90fcd
kiprotichgidii <gedionkiprotich3@gmail.com>
2025-12-15 13:05:02 +0300

    Update providers

--- modules/proxmox-vm/providers.tf
+++ modules/proxmox-vm/providers.tf
@@ -1,8 +1,9 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "telmate/proxmox"
-      version = "= 3.0.2-rc06"
-    }
-  }
+provider "proxmox" {
+  pm_api_url          = var.proxmox_api_url
+  pm_user             = var.proxmox_user
+  pm_password         = var.proxmox_password
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure     = var.proxmox_tls_insecure
+  pm_timeout          = 600
 }
