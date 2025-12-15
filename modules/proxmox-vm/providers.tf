1ec5f2ac7348d177728bd4a905efd728079274bd
kiprotichgidii <gedionkiprotich3@gmail.com>
2025-12-15 13:01:01 +0300

    Update providers

--- modules/proxmox-vm/providers.tf
+++ modules/proxmox-vm/providers.tf
@@ -1,25 +1,8 @@
-# Provider Configurations
 terraform {
   required_providers {
     proxmox = {
       source  = "telmate/proxmox"
       version = "= 3.0.2-rc06"
-    }
-
-    local = {
-      source = "hashicorp/local"
-    }
-
-    tls = {
-      source = "hashicorp/tls"
-    }
-
-    random = {
-      source = "hashicorp/random"
-    }
-
-    template = {
-      source = "hashicorp/template"
     }
   }
 }
