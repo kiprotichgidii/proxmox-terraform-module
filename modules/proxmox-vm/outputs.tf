c975a75728f7f23db92df3a242a7685bb91eed44
kiprotichgidii <gedionkiprotich3@gmail.com>
2025-12-28 18:53:46 +0300

    Add increment in outputs

--- modules/proxmox-vm/outputs.tf
+++ modules/proxmox-vm/outputs.tf
@@ -1,38 +1,59 @@
 output "vmid" {
   description = "The ID of the created virtual machine"
-  value       = try(proxmox_vm_qemu.qemu_vm[0].vmid)
+  value = {
+    for idx, vm in proxmox_vm_qemu.qemu_vm :
+    vm.name => try(vm.vmid)
+  }
 }
 
 output "name" {
-  value       = try(proxmox_vm_qemu.qemu_vm[0].name)
   description = "The name of the created virtual machine"
+  value = {
+    for idx, vm in proxmox_vm_qemu.qemu_vm :
+    vm.name => try(vm.name)
+  }
 }
 
 output "node" {
-  value       = try(proxmox_vm_qemu.qemu_vm[0].target_node)
   description = "The node the VM was created on"
+  value = {
+    for idx, vm in proxmox_vm_qemu.qemu_vm :
+    vm.name => try(vm.target_node)
+  }
 }
 
 output "status" {
-  value       = try(proxmox_vm_qemu.qemu_vm[0].vm_state)
   description = "The status of the QEMU VM"
+  value = {
+    for idx, vm in proxmox_vm_qemu.qemu_vm :
+    vm.name => try(vm.vm_state)
+  }
 }
 
 output "ssh_user" {
   description = "The SSH user for the VM"
-  value       = var.cloudinit.user_name
+  value = {
+    for idx, vm in proxmox_vm_qemu.qemu_vm :
+    vm.name => try(var.cloudinit.user_name)
+  }
 }
 
 output "ssh_user_password" {
   description = "The SSH password for the VM (if set to be generated)"
-  value       = var.cloudinit.set_user_password ? random_password.user_password[0].result : ""
-  sensitive   = true
+  value = {
+    for idx, vm in proxmox_vm_qemu.qemu_vm :
+    vm.name => try(var.cloudinit.set_user_password ? random_password.user_password[0].result : "")
+  }
+  sensitive = true
 }
 
 output "root_password" {
   description = "The root password for the VM (if set to be generated)"
-  value       = var.cloudinit.set_root_password ? random_password.root_password[0].result : ""
-  sensitive   = true
+  value = {
+    for idx, vm in proxmox_vm_qemu.qemu_vm :
+    vm.name => try(var.cloudinit.set_root_password ? random_password.root_password[0].result : "")
+  }
+  sensitive = true
 }
 
 output "vm_ip_addresses" {
