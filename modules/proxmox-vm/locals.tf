6c899862af60246b0dd6ae08eaea2c4058cf993c
kiprotichgidii <gedionkiprotich3@gmail.com>
2025-12-28 19:31:43 +0300

    Add IP calculation to locals.tf

--- modules/proxmox-vm/locals.tf
+++ modules/proxmox-vm/locals.tf
@@ -11,4 +11,26 @@ locals {
   # SSH connection
   generated_ssh_key = var.generate_ssh_key ? [trimspace(tls_private_key.ssh_key[0].public_key_openssh)] : []
   combined_ssh_keys = concat(var.ssh_keys, local.generated_ssh_key)
+
+  # ============================================================
+  #  IP Address Calculation
+  # ============================================================
+
+  # Parse the provided IP address (e.g., "192.168.1.130/24")
+  ip_cidr_split = split("/", var.cloudinit.ip_address)
+  ip_address    = local.ip_cidr_split[0]
+  cidr_suffix   = length(local.ip_cidr_split) > 1 ? local.ip_cidr_split[1] : "24"
+  ip_parts      = split(".", local.ip_address)
+
+  # Generate list of IPs by incrementing the last octet
+  generated_ips = [
+    for i in range(var.vm_count) :
+    format("%s.%s.%s.%d/%s",
+      local.ip_parts[0],
+      local.ip_parts[1],
+      local.ip_parts[2],
+      tonumber(local.ip_parts[3]) + i,
+      local.cidr_suffix
+    )
+  ]
 }
