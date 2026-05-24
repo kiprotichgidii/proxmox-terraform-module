60019459612a4c67f378bcf3d01ef880383fb9f0
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-05-24 10:59:58 +0300

    Add VLAN tag variable.

--- modules/proxmox-vm/variables.tf
+++ modules/proxmox-vm/variables.tf
@@ -128,7 +128,7 @@ variable "networks" {
     id       = string
     bridge   = string
     model    = string
-    tag      = number
+    tag      = int
     macaddr  = optional(string, null)
     firewall = optional(bool, false)
   }))
